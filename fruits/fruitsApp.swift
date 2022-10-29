88b3bf638a70fd140109719bf5ce8a01dc9ba093
OnurrUgur <onurugur.prf@gmail.com>
2022-10-29 21:21:29 +0300

    Initial Commit

---
+++ fruits/fruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruitsApp.swift
+//  fruits
+//
+//  Created by Onur Uğur on 29.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct fruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
