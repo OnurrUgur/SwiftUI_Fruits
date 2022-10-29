380d5aadcde19073752af2fd1b1c5c9a2969f43d
OnurrUgur <onurugur.prf@gmail.com>
2022-10-29 22:34:06 +0300

    Setup for project

---
+++ fruits/fruits/OnBoardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoardingView.swift
+//  fruits
+//
+//  Created by Onur Uğur on 29.10.2022.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingView()
+    }
+}
